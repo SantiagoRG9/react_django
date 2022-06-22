deeef0292a753e762534970eec226bbabac67c8d
Santiago <santyra3030@gmail.com>
2022-06-22 09:45:27 -0500

    reconstruccion del docker

--- api_django/api_django/settings/base.py
+++ api_django/api_django/settings/base.py
@@ -51,9 +51,6 @@ SWAGGER_SETTINGS = {
     'DOC_EXPANSION': 'none'
 }
 
-TOKEN_EXPIRED_AFTER_SECONDS = 80000
-
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> y santiago.
+          Editar rewtert t rtyrty rewqrewr <code>src/App.js</code>gfhgfh santiago.
         </p>
         <a
           className="App-link"
